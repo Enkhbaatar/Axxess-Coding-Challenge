977aaf2a946f58114a5b774b27e397920aae3649
Enkhbaatar <enhee.day@gmail.com>
2019-08-07 22:56:21 -0500

    Created Application, application structure(packages)and Network part.

---
+++ app/src/main/java/com/enkhee/codingchallenge/data/network/CodingChallengeApiService.kt
@@ -0,0 +1,47 @@
+package com.enkhee.codingchallenge.data.network
+
+import com.enkhee.codingchallenge.utils.API_KEY
+import io.reactivex.schedulers.Schedulers
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+
+interface CodingChallengeApiService {
+
+    companion object {
+        operator fun invoke(connectivityInterceptor: ConnectivityInterceptor): CodingChallengeApiService {
+            val requestInterceptor = Interceptor{chain ->
+                val url = chain.request()
+                    .url()
+                    .newBuilder()
+                    .addQueryParameter("key", API_KEY)
+                    .build()
+                val request = chain.request()
+                    .newBuilder()
+                    .url(url)
+                    .build()
+                return@Interceptor chain.proceed(request)
+            }
+
+            val logging = HttpLoggingInterceptor()
+            logging.level = HttpLoggingInterceptor.Level.BODY
+
+            val okHttpClient = OkHttpClient.Builder()
+                .addInterceptor(requestInterceptor)
+                .addInterceptor(connectivityInterceptor)
+                .addInterceptor(logging)
+                .build()
+
+            return Retrofit.Builder()
+                .client(okHttpClient)
+                .baseUrl("https://api.apixu.com/v1/")
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.createWithScheduler(Schedulers.io()))
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+                .create(CodingChallengeApiService::class.java)
+        }
+    }
+}
---
+++ app/src/main/java/com/enkhee/codingchallenge/internal/Exceptions.kt
@@ -0,0 +1,5 @@
+package com.enkhee.codingchallenge.internal
+
+import java.io.IOException
+
+class NoConnectivityException: IOException()
